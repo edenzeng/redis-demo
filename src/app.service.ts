d9136c82923df6772509d7cf6dd09ff7272db418
edenzeng <99775489+edenzeng@users.noreply.github.com>
2023-06-01 10:41:36 +0800

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,23 +1,24 @@
-import { InjectCluster, InjectRedis } from '@liaoliaots/nestjs-redis';
+import { InjectRedis } from '@liaoliaots/nestjs-redis';
 import { Injectable } from '@nestjs/common';
-import Redis, { Cluster } from 'ioredis';
+import Redis from 'ioredis';
 
 @Injectable()
 export class AppService {
   constructor(
-    @InjectRedis() private readonly redis: Redis, // or // @InjectRedis(DEFAULT_REDIS_NAMESPACE) private readonly redis: Redis
-    // @InjectCluster() private readonly redis: Cluster, // or // @InjectCluster(DEFAULT_CLUSTER_NAMESPACE) private readonly cluster: Cluster
+    @InjectRedis() private readonly redis: Redis, // or // @InjectRedis(DEFAULT_REDIS_NAMESPACE) private readonly redis: Redis // @InjectCluster() private readonly redis: Cluster, // or // @InjectCluster(DEFAULT_CLUSTER_NAMESPACE) private readonly cluster: Cluster
   ) {}
 
   async get() {
-    return await this.redis.get('test');
+    return await this.redis.get('zxe432');
   }
 
   async set() {
-    return await this.redis.set('test', new Date() + '', 'EX', 10 * 60);
+    return await this.redis.set('zxe432', new Date() + '', 'EX', 10 * 60);
   }
 
   getHello(): string {
     return 'Hello World!';
   }
+
+  
 }
